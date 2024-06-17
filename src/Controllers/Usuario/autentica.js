83e8f44e1f517060ea5d4163d768e7d934be2a5d
Lipe <lipecasimiro@gmail.com>
2024-06-17 00:55:19 -0300

    Liga novas tabelas aos usuarios cadastrados.

--- src/Controllers/Usuario/autentica.js
+++ src/Controllers/Usuario/autentica.js
@@ -32,6 +32,12 @@ exports.salvaSignup = (req, res) => {
     
             const queryCultura = 'INSERT INTO tb_cultura () VALUES ()'
             conexao.query(queryCultura)
+
+            const queryAI = 'INSERT INTO tb_ai () VALUES ()'
+            conexao.query(queryAI)
+
+            const queryHistorico = 'INSERT INTO tb_historicoplantio () VALUES ()'
+            conexao.query(queryHistorico)
             console.log('Usuário cadastrado com sucesso:', results);
             return res.status(201).json({ message: 'Usuário cadastrado com sucesso.' });
         });
